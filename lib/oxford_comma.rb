82d49565b3091da4ff6bde29a5f9b84f3c101844
Alex Merecka <merecka@gmail.com>
2019-02-13 18:51:46 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,8 +2,10 @@ def oxford_comma(array)
   if array.length <= 1
     array.join
     
- ## elseif array.length == 2
- ##   array.join(" and ")
+  elseif array.length == 2
+    array.join(" and ")
+    
+  else
   end
   
 end
